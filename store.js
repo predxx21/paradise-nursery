bea174d4de8221d0d61310dee1b47d660e1b5f6b
predxx21 <0332221025@unjfsc.edu.pe>
2026-03-08 01:22:13 -0500

    Update store.js

--- store.js
+++ store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from '../features/cart/CartSlice';
+import cartReducer from './features/cart/CartSlice'; // Ajusta la ruta según tu estructura
 
 export const store = configureStore({
   reducer: {
